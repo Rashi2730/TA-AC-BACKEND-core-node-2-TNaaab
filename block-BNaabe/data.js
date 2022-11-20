2885d1e481f63cd4dbfac6c1fa645bd7f5bba075
Rashi Israni <rashiisrani97@gmail.com>
2022-11-21 03:23:54 +0530

    node js assgn1

---
+++ block-BNaabe/data.js
@@ -0,0 +1,25 @@
+var http = require('http');
+var qs = require('querystring');
+var server = http.createServer(handleReq);
+
+function handleReq(req, res) {
+  var store = '';
+  var dataFormat = req.headers['content-type'];
+  req.on('data', (chunk) => {
+    store += chunk;
+  });
+  req.on('end', () => {
+    if (dataFormat === 'application/json') {
+      var parseddata = JSON.parse(store);
+      res.end(store);
+    }
+    if (dataFormat === 'application/x-www-form-urlencoded') {
+      var parseddata = qs.parse(store);
+      res.end(JSON.stringify(parseddata));
+    }
+  });
+}
+
+server.listen(9000, () => {
+  console.log('server listening to port 9k');
+});
